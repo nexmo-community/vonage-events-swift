53dcf3bfa9e1c193912d6906bc7f1cfddbf897a6
Abdul Ajetunmobi <abdul.ajetunmobi@vonage.com>
2021-02-18 16:04:37 +0000

    stop crash and add backlog for messages

--- Sources/App/SocketController.swift
+++ Sources/App/SocketController.swift
@@ -12,6 +12,7 @@ struct Socket {
 
 final class SocketController {
     var sockets: [Socket] = []
+    var backlog: [String: [String]] = [:]
 
     func addSocket(_ socket: Socket) {
         self.sockets.append(socket)
@@ -22,7 +23,18 @@ final class SocketController {
     }
 
     func sendSocketMessage(with appIdString: String, message: String) {
-        let socket = sockets.first { $0.appId.description == appIdString }!
-        socket.ws.send(message)
+        if let socket = sockets.first(where: { $0.appId.description == appIdString }) {
+            if let backlogMessages = backlog[appIdString] {
+                for backlogMessage in backlogMessages {
+                    socket.ws.send(backlogMessage)
+                    backlog[appIdString] = nil
+                }
+            }
+            socket.ws.send(message)
+        } else {
+            var backlogMessages: [String] = backlog[appIdString] ?? []
+            backlogMessages.append(message)
+            backlog[appIdString] = backlogMessages
+        }
     }
 }
